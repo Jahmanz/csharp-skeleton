bd8c3aeb939c784a5fb6b7b5ef9d7851ed0eb864
Guest User <Guest@epicodus-10.local>
2018-02-07 11:14:28 -0800

    skeleton for C# projects

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
     public class HomeController : Controller
-    {
-        public string Hello() { return "Hello friend!"; }
+
+  {
+
+    [Route("/form")]
+        public ActionResult Form()
+        {
+          return View();
+        }
+        [Route("/greeting_card")]
+        public ActionResult GreetingCard()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient(Request.Query["recipient"]);
+            myLetterVariable.SetSender(Request.Query["sender"]);
+            return View("Hello", myLetterVariable);
+        }
     }
-}
+  }
+
+
+
+
+
+  // [Route("/")]
+  // public ActionResult Letter()
+  // {
+  //   LetterVariable myLetterVariable = new LetterVariable();
+  //   myLetterVariable.SetRecipient("Eric");
+  //   myLetterVariable.SetSender("John");
+  //   return View(myLetterVariable);
+  // }
+
+
+
+
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace FriendLetter.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+//       [Route("/")]
+//        public ActionResult Letter()
+//        {
+//          return View();
+//        }
+//        [Route("/journal")]
+//         public ActionResult Journal()
+//         {
+//           return View();
+//         }
+//
+//     }
+// }
